73107bcf1f8f138f8cc7bcc14a91f6b7e790a4c4
helitonns <helitonns@gmail.com>
2026-01-16 16:19:51 -0400

    Criando o Schema do Appointment Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,10 @@ export default async function Home() {
         ))}
       </div>
 
-      <div>
+      <div className="
+        fixed bottom-0 left-0 right-0 flex justify-center bg-background-tertiary py-4.5 px-6
+        md:botton-6 md:right-6 md:left-auto md:top-auto md:w-auto md:bg-transparent md:p-0 md:mt-4 md:mb-4"
+      >
         <AppointmentForm />
       </div>
     </div>
--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -2,8 +2,34 @@
 
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const appointmentFormSchema = z.object({
+  tutorName: z.string().min(3, "O nome do tutor deve ter no mínimo 3 caracteres"),
+  petName: z.string().min(3, "O nome do pet deve ter no mínimo 3 caracteres"),
+  phone: z.string().min(11, "O telefone deve ter no mínimo 11 carcteres"),
+  description: z.string().min(3, "A descrição deve ter no mínimo 3 carcteres"),
+});
+
+type AppointmentFormValues = z.infer<typeof appointmentFormSchema>;
 
 export function AppointmentForm() {
+  const form = useForm<AppointmentFormValues>({
+    resolver: zodResolver(appointmentFormSchema),
+    defaultValues: {
+      tutorName: "",
+      petName: "",
+      phone: "",
+      description: "",
+    }
+  });
+
+  function onSubmit(data: AppointmentFormValues) {
+    console.log(data);
+  }
 
   return (
     <Dialog>
@@ -11,11 +37,19 @@ export function AppointmentForm() {
         <Button variant="brand">Novo Agendamento</Button>
       </DialogTrigger>
 
-      <DialogContent>
+      <DialogContent variant="appointment" overlayVariant="blurred" showCloseButton>
         <DialogHeader>
-          <DialogTitle>Agende um atendimento</DialogTitle>
-          <DialogDescription>Preencha os dados do cliente para realizar o agendamento:</DialogDescription>
+          <DialogTitle size="modal" className="text-center">Agende um atendimento</DialogTitle>
+          <DialogDescription size="modal" className="text-center">Preencha os dados do cliente para realizar o agendamento:</DialogDescription>
         </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <input {...form.register('tutorName')} type="text" />
+
+            <Button type="submit">Salvar</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
